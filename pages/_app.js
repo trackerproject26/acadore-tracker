2a426efaa666d3b47e4a1c56a78e3ae58a876e8c
trackerproject26 <trackerproject26@gmail.com>
2026-05-24 01:37:34 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabase';
 
 export default function App({ Component, pageProps }) {
+  const [session, setSession] = useState(undefined);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (session === undefined) return null;
+
   return (
     <>
       <Head>
@@ -12,7 +28,7 @@ export default function App({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} session={session} />
     </>
   );
 }
